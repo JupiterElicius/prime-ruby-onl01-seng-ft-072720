81ed35ae4f694a2fed50d90390cd607c72c4365f
Nick Bazaldua <bazalnick@gmail.com>
2020-08-02 04:37:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime? (number) #create method that takes an argument
   return false if number < 2  # any number less than two is not prime
   return true if number == 2 # 2 is a prime number
-  (2..number-1).each {|int| return false if number % int == 0}#this takes 
+  (2..number-1).each {|int| return false if number % int == 0}#this takes every value within the range and if that number has a remainder greater than 0 it will show false because it's not prime
   true
 end
