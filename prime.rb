35b9ba2d923d2e9bf840c4a43611c3f699943a3b
Nick Bazaldua <bazalnick@gmail.com>
2020-08-02 04:34:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,6 @@
-# Add  code here!
+def prime? (number) #create method that takes an argument
+  return false if number < 2  #force early return for efficiency if any number is <2 (1 and negative numbers are not prime)
+  return true if number == 2 # 2 is a prime number
+  (2..number-1).each  
+  true
+end
