efaf59ccb87ca42e72cae15a24bb126e9e7ade97
Nick Bazaldua <bazalnick@gmail.com>
2020-08-02 04:35:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime? (number) #create method that takes an argument
-  return false if number < 2  #force early return for efficiency if any number is <2 (1 and negative numbers are not prime)
+  return false if number < 2  # any number less than two is not prime
   return true if number == 2 # 2 is a prime number
   (2..number-1).each  
   true
